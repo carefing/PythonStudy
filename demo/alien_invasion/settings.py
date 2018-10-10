7b30f2dd3d7df44822ca4d95c0a157a87b8d5576
Yolanda.Y.Zhu <zhuying6886@163.com>
2018-10-10 17:19:50 +0800

    demo(alien_invasion): add motionless aliens

--- demo/alien_invasion/settings.py
+++ demo/alien_invasion/settings.py
@@ -4,8 +4,8 @@ class Settings(object):
     def __init__(self):
         """初始化游戏的设置"""
         # 屏幕设置
-        self.screen_width = 400
-        self.screen_height = 300
+        self.screen_width = 600
+        self.screen_height = 400
         self.bg_color = (230, 230, 230)
 
         # 飞船的设置
