e0d177c87e10b6352b71b7c5a65a920730963953
Yolanda.Y.Zhu <zhuying6886@163.com>
2018-10-19 11:36:46 +0800

    example(pygal): data analysis of python projects - add customized hint

--- pygal_examples/github_repo/python_repos.py
+++ pygal_examples/github_repo/python_repos.py
@@ -15,10 +15,22 @@ print("Total repositories:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 print("Repositories returned:", len(repo_dicts))
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    if repo_dict['description']:
+        plot_dict = {
+            'value': repo_dict['stargazers_count'],
+            'label': repo_dict['description'],
+            'xlink': repo_dict['html_url'],
+        }
+    else:
+        plot_dict = {
+            'value': repo_dict['stargazers_count'],
+            'label': 'No description',
+            'xlink': repo_dict['html_url'],
+        }
+    plot_dicts.append(plot_dict)
 
 # 可视化
 my_style = LS('#336699', base_style=LCS)
@@ -37,8 +49,9 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
-chart.render_to_file('python_repos.svg')
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos_des.svg')
+# chart.render_to_file('python_repos.svg')
 
 # print("\nSelected information about each repository:")
 # for repo_dict in repo_dicts:
